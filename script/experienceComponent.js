c2e0e6e11768974d8ee35e0f5115223447a15246
Alucard <85690382+Alucard2169@users.noreply.github.com>
2022-11-04 20:22:41 +0530

    optimize code

--- script/experienceComponent.js
+++ script/experienceComponent.js
@@ -2,44 +2,49 @@
 
 const experienceSection = document.getElementById('experience');
 
-const addExperience = (id,head,link,time,para) => {
-    const experColumn = document.createElement('div');
-    const experHead = document.createElement('h4');
-    const experLinkIcon = document.createElement('i');
-    const experLink = document.createElement('a');
-    const experTime = document.createElement('span');
-    const experPara = document.createElement('p');
-    const linkIcon = document.createElement('img');
- 
+const addExperience = (items) => {
+    console.log(items)
+    items.forEach((item) => {
+        const { id, companyName,link, time, about } = item;
+    
+            const experColumn = document.createElement('div');
+            const experHead = document.createElement('h4');
+            const experLinkIcon = document.createElement('i');
+            const experLink = document.createElement('a');
+            const experTime = document.createElement('span');
+            const experPara = document.createElement('p');
+            const linkIcon = document.createElement('img');
+        
 
 
 
-    experColumn.setAttribute('id',id)
-    experLink.textContent = head;
-    experLink.href = link;
-    experLink.setAttribute('target', '_blank');
-    experTime.textContent = time;
-    experPara.textContent = para;
-    linkIcon.classList.add('link');
-    linkIcon.src = './assets/icons/link_icon.svg';
+            experColumn.setAttribute('id',id)
+            experLink.textContent = companyName;
+            experLink.href = link;
+            experLink.setAttribute('target', '_blank');
+            experTime.textContent = time;
+            experPara.textContent = about;
+            linkIcon.classList.add('link');
+            linkIcon.src = './assets/icons/link_icon.svg';
 
-    experHead.appendChild(linkIcon)
-
-
-    experColumn.classList.add('exper');
-    experHead.classList.add('Head');
-    experTime.classList.add('Time');
-    experPara.classList.add('About');
+            experHead.appendChild(linkIcon)
 
 
-    experLink.appendChild(experLinkIcon)
-    experHead.appendChild(experLink);
-    experColumn.appendChild(experHead);
-    experColumn.appendChild(experTime);
-    experColumn.appendChild(experPara)
+            experColumn.classList.add('exper');
+            experHead.classList.add('Head');
+            experTime.classList.add('Time');
+            experPara.classList.add('About');
 
 
-    experienceSection.appendChild(experColumn)
+            experLink.appendChild(experLinkIcon)
+            experHead.appendChild(experLink);
+            experColumn.appendChild(experHead);
+            experColumn.appendChild(experTime);
+            experColumn.appendChild(experPara)
+
+
+            experienceSection.appendChild(experColumn)
+        })
 
 }
 
